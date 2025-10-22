1364ed7c1eb31aa111613aaf443fd95e62b00f32
kankara1892 <kankara189@gmail.com>
2025-10-22 15:01:46 +0900

    速度制限追加
    magnitudeからの数値で制限する

--- Assets/Playercontroller.cs
+++ Assets/Playercontroller.cs
@@ -6,13 +6,14 @@ public class Playercontroller : MonoBehaviour
 {
     [SerializeField,Tooltip("加速度")]
     float speed = 25f;
-    [SerializeField,Tooltip("入力に対する追従度")]
-    float moveForceMultiplier;
+    [SerializeField]
+    float _maxMagunitude;
     float Horizontal;
     float Vertical;
     Rigidbody rb;
     Vector3 Player_pos;
     Vector3 diff;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -27,18 +28,24 @@ public class Playercontroller : MonoBehaviour
 
     private void FixedUpdate()
     {
-        diff = new Vector3(transform.position.x, 0, transform.position.z)
-                - (new Vector3(Player_pos.x, 0, Player_pos.z));
-        Player_pos = transform.position;
         //player進行方向回転
-        if (diff.magnitude > 0.001f)
+        diff = new Vector3(transform.position.x, 0, transform.position.z)
+                - new Vector3(Player_pos.x, 0, Player_pos.z);
+
+        Player_pos = transform.position;
+
+        if (diff.magnitude > 0.01f)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation,
                 Quaternion.LookRotation(diff), 0.2f);
         }
+
         //移動
         Vector3 force = Vector3.zero;
-        force = new Vector3(Horizontal*speed,0,Vertical* speed);
-        rb.AddForce(force);
+        force =new Vector3 (Horizontal,0,Vertical)*speed;
+        if(rb.velocity.magnitude <= _maxMagunitude)
+        {
+            rb.AddForce(force);
+        }
     }
 }
