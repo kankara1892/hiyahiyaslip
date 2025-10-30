fbd22be9dab0ebd78cc7670480477c0f67c1eb85
kankara1892 <kankara189@gmail.com>
2025-10-30 14:58:42 +0900

    boost処理修正
    まだ完成ではない
    判定が取れる
    加速反応してる

--- Assets/Scripts/flootdetection.cs
+++ Assets/Scripts/flootdetection.cs
@@ -6,26 +6,44 @@ public class floorditecction : MonoBehaviour
 {
     [SerializeField] Rigidbody _rigidbody = default;
     [SerializeField] float _boostSpeed = default;
+    [SerializeField] float _boostCoolTime = default;
     RaycastHit _groundHit;
     bool _groundHitDetect;
     Vector3 _movementDifference;
     Vector3 Player_pos;
+    string _groundHitTag;
+    float _boostCooldownTimer;
+    const string BOOSTFLOOR = "Boostfloor";
+    const string GROUND = "Ground";
     private void Update()
     {
-        if (_groundHitDetect & _groundHit.collider.tag == ("BoostFloor"))
+        _boostCooldownTimer += Time.deltaTime;
+        
+        if(_groundHitDetect )
         {
-            Boost();
+            _groundHitTag = _groundHit.collider.tag;
+        }
+        if (_groundHitTag == BOOSTFLOOR)
+        {
+            if (_boostCoolTime <= _boostCooldownTimer)
+            {
+                Boost();
+            }
+            
         }
     }
 
     private void OnDrawGizmos()
     {
-        _groundHitDetect = Physics.BoxCast(transform.position + Vector3.down, Vector3.one * 0.5f, Vector3.down, out _groundHit, transform.rotation);
+        _groundHitDetect = Physics.BoxCast(transform.position + Vector3.down, Vector3.one * 0.5f, Vector3.down, 
+                                            out _groundHit, transform.rotation,LayerMask.GetMask(GROUND));
         Gizmos.DrawWireCube(transform.position + Vector3.down, Vector3.one);
-        Debug.Log(_groundHit.collider.tag);
+       
     }
     private void Boost()
     {
+        Debug.Log("Boost");
+        _boostCooldownTimer = 0;
         Player_pos = transform.position;
         _movementDifference = new Vector3(transform.position.x, 0, transform.position.z)
                 - new Vector3(Player_pos.x, 0, Player_pos.z);
